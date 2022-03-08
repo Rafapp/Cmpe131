c6a1b7d33c908499775fc11f804b44c50e527421
Rafa <rpadiper@gmail.com>
2022-03-08 14:28:03 -0800

    document analyzer ready for testing

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,8 +1,26 @@
-from collections import Counter
-import re
-import collections
-
 file = open('document.txt', encoding='utf8')
 text = file.read()
 file.close
-print(collections.Counter(text.split(' ')).most_common)
+
+def CountRepetitions(theText):
+    theText = theText.split(" ")
+    uniqueText = []
+    solDict = {}
+
+    for word in theText:
+        if word not in uniqueText:
+            uniqueText.append(word)
+        solDict[word] = CountAppearances(word, theText)
+    return solDict
+
+def CountAppearances(aWord, aText):
+    count = 0
+    for word in aText:
+        if(word == aWord):
+            count += 1
+    return count
+
+solution = sorted(CountRepetitions(text).items(), key=lambda x: x[1], reverse=True)
+
+for key in solution:
+    print(key[0] + ": " + str(key[1]))
