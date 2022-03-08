bed065a0e65a618537c45b88d462dfe19d7ba7e0
Rafa <rpadiper@gmail.com>
2022-03-08 12:41:38 -0800

    fixed sort

--- sort.py
+++ sort.py
@@ -2,14 +2,15 @@ def sort_list(list):
     n = len(list)
     i = 0
     while(i < n):
-        while(i < n - i - 1):
-            if(list[i] > list[i + 1]):
-                temp = list[i]
-                list[i] = list[i+1] 
-                list[i+1] = temp
-            i += 1
+        j = 0
+        while(j < n-i-1):
+            if(list[j] > list[j+1]):
+                tmp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = tmp
+            j += 1
         i += 1
     return list
 
-test = [3,2,4,89]
+test = [4,6,7,8,3,4,2,5,8,3,1,4,7,8,0,4,6]
 print(sort_list(test))
